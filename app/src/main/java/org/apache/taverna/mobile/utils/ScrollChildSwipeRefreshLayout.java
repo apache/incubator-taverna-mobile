9f4a246daec57d4495a2acc49fb8879b41504dfe
Sagar <kumarsagar15795@gmail.com>
2016-05-06 16:55:47 +0530

    custom swipe refresh layout

---
+++ app/src/main/java/org/apache/taverna/mobile/utils/ScrollChildSwipeRefreshLayout.java
@@ -0,0 +1,32 @@
+package org.apache.taverna.mobile.utils;
+
+import android.content.Context;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ScrollChildSwipeRefreshLayout extends SwipeRefreshLayout {
+
+    private View mScrollUpChild;
+
+    public ScrollChildSwipeRefreshLayout(Context context) {
+        super(context);
+    }
+
+    public ScrollChildSwipeRefreshLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean canChildScrollUp() {
+        if (mScrollUpChild != null) {
+            return ViewCompat.canScrollVertically(mScrollUpChild, -1);
+        }
+        return super.canChildScrollUp();
+    }
+
+    public void setScrollUpChild(View view) {
+        mScrollUpChild = view;
+    }
+}
