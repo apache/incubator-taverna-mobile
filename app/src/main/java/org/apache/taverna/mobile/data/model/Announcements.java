5b5f64430bce01da284188c5e6615ce727e12eaa
Sagar <kumarsagar15795@gmail.com>
2016-05-06 17:03:17 +0530

    implement simple-xml annotation in model class

--- app/src/main/java/org/apache/taverna/mobile/data/model/Announcements.java
+++ app/src/main/java/org/apache/taverna/mobile/data/model/Announcements.java
@@ -1,21 +1,60 @@
 package org.apache.taverna.mobile.data.model;
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.ElementList;
+import org.simpleframework.xml.Root;
 
+import java.util.List;
+
+@Root(name = "announcements")
 public class Announcements {
-    private Announcement[] announcement;
 
-    public Announcement[] getAnnouncement ()
-    {
-        return announcement;
-    }
+    @ElementList(name = "announcement", inline = true, required = false)
+    List<Announcement> announcement;
 
-    public void setAnnouncement (Announcement[] announcement)
-    {
-        this.announcement = announcement;
-    }
 
-    @Override
-    public String toString()
-    {
-        return "ClassPojo [announcement = "+announcement+"]";
+
+
+    public List<Announcement> getAnnouncement() { return this.announcement; }
+    public void setAnnouncement(List<Announcement> _value) { this.announcement = _value; }
+
+
+
+    public static class Announcement {
+
+        @Attribute(name="resource", required = false)
+        String resource;
+
+
+        @Attribute(name="uri", required = false)
+        String uri;
+
+
+        @Attribute(name="id", required = false)
+        String id;
+
+        @Element(name="announcement",required = false)
+        String content;
+
+        public String getContent() {
+            return content;
+        }
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+
+        public String getResource() { return this.resource; }
+        public void setResource(String _value) { this.resource = _value; }
+
+
+        public String getUri() { return this.uri; }
+        public void setUri(String _value) { this.uri = _value; }
+
+
+        public String getId() { return this.id; }
+        public void setId(String _value) { this.id = _value; }
+
+
     }
 }
