cec1bc7344fc83f0ffb0e3610b751aef6eb1cdd5
Sagar <kumarsagar15795@gmail.com>
2016-05-06 16:46:12 +0530

    add pagination query

--- app/src/main/java/org/apache/taverna/mobile/data/remote/TavernaService.java
+++ app/src/main/java/org/apache/taverna/mobile/data/remote/TavernaService.java
@@ -3,11 +3,12 @@ package org.apache.taverna.mobile.data.remote;
 import org.apache.taverna.mobile.data.model.Announcements;
 
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 import rx.Observable;
 
 public interface TavernaService {
 
     @GET("/announcements.xml")
-    Observable<Announcements> getAllAnnouncements();
+    Observable<Announcements> getAllAnnouncements(@Query("page") int pageNumber);
 
 }
