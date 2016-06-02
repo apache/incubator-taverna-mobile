4e32c2d256cb323687303265d49dda660140e431
Sagar <kumarsagar15795@gmail.com>
2016-06-02 18:04:49 +0530

    reformatted POJO

--- app/src/main/java/org/apache/taverna/mobile/data/model/Announcement.java
+++ app/src/main/java/org/apache/taverna/mobile/data/model/Announcement.java
@@ -1,7 +1,9 @@
 package org.apache.taverna.mobile.data.model;
 
+import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
+import org.simpleframework.xml.Text;
 
 /**
  * Created by Sagar
@@ -10,7 +12,7 @@ import org.simpleframework.xml.Root;
 public class Announcement {
 
     @Element(name = "author")
-    private String author;
+    private author author;
 
     @Element(name = "title")
     private String title;
@@ -21,12 +23,46 @@ public class Announcement {
     @Element(name = "created-at")
     private String date;
 
-    public String getAuthor() {
+    @Attribute(name="resource", required = false)
+    String resource;
+
+
+    @Attribute(name="uri", required = false)
+    String uri;
+
+
+    @Attribute(name="id", required = false)
+    String id;
+
+    @Element(name = "id")
+    private String idElement;
+
+    public String getIdElement() {
+        return idElement;
+    }
+
+    public void setIdElement(String idElement) {
+        this.idElement = idElement;
+    }
+
+    public String getResource() { return this.resource; }
+    public void setResource(String _value) { this.resource = _value; }
+
+
+    public String getUri() { return this.uri; }
+    public void setUri(String _value) { this.uri = _value; }
+
+
+    public String getId() { return this.id; }
+    public void setId(String _value) { this.id = _value; }
+
+
+    public author getAuthor() {
         return author;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
+    public void setAuthor(author author1) {
+        this.author = author1;
     }
 
     public String getTitle() {
@@ -51,6 +87,44 @@ public class Announcement {
 
     public void setDate(String date) {
         this.date = date;
+    }
+
+    public static class author {
+
+        @Attribute(name="resource", required = false)
+        String resource;
+
+
+        @Attribute(name="uri", required = false)
+        String uri;
+
+
+        @Attribute(name="id", required = false)
+        String id;
+
+        @Text
+        String content;
+
+        public String getContent() {
+            return content;
+        }
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+
+        public String getResource() { return this.resource; }
+        public void setResource(String _value) { this.resource = _value; }
+
+
+        public String getUri() { return this.uri; }
+        public void setUri(String _value) { this.uri = _value; }
+
+
+        public String getId() { return this.id; }
+        public void setId(String _value) { this.id = _value; }
+
+
     }
 }
 
