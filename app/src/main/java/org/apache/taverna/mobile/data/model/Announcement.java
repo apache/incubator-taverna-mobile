adb5670d12d11095fb3cf24fc533b7cc905b726a
Sagar <kumarsagar15795@gmail.com>
2016-06-02 15:55:49 +0530

    add Detail Announcement Retrofit service and reformatted pojo

--- app/src/main/java/org/apache/taverna/mobile/data/model/Announcement.java
+++ app/src/main/java/org/apache/taverna/mobile/data/model/Announcement.java
@@ -4,19 +4,22 @@ import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
 
 /**
- * Created by Admin on 02/06/16.
+ * Created by Sagar
  */
-@Root(name="announcement")
+@Root(name = "announcement")
 public class Announcement {
 
-    @Element(name="author")
+    @Element(name = "author")
     private String author;
 
-    @Element(name="title")
+    @Element(name = "title")
     private String title;
 
-    @Element(name="text")
+    @Element(name = "text")
     private String text;
+
+    @Element(name = "created-at")
+    private String date;
 
     public String getAuthor() {
         return author;
@@ -40,6 +43,14 @@ public class Announcement {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 }
 
--- app/src/main/java/org/apache/taverna/mobile/data/remote/TavernaService.java
+++ app/src/main/java/org/apache/taverna/mobile/data/remote/TavernaService.java
@@ -1,14 +1,21 @@
 package org.apache.taverna.mobile.data.remote;
 
+import org.apache.taverna.mobile.data.model.Announcement;
 import org.apache.taverna.mobile.data.model.Announcements;
 
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 import rx.Observable;
 
+/**
+ * Created by Sagar
+ */
+
 public interface TavernaService {
 
     @GET("/announcements.xml")
     Observable<Announcements> getAllAnnouncements(@Query("page") int pageNumber);
 
+    @GET("/announcement.xml")
+    Observable<Announcement> getAnnouncement(@Query("id") int id);
 }
