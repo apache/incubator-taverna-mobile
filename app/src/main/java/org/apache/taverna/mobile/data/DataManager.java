5690ec2ea1eeddfb43b2957a58891db89f477e2a
Sagar <kumarsagar15795@gmail.com>
2016-06-16 00:11:25 +0530

    add DataManager for getting all Workflow

--- app/src/main/java/org/apache/taverna/mobile/data/DataManager.java
+++ app/src/main/java/org/apache/taverna/mobile/data/DataManager.java
@@ -1,14 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.taverna.mobile.data;
 
 import org.apache.taverna.mobile.data.model.Announcements;
 import org.apache.taverna.mobile.data.model.DetailAnnouncement;
+import org.apache.taverna.mobile.data.model.Workflows;
 import org.apache.taverna.mobile.data.remote.BaseApiManager;
 
 import rx.Observable;
 
-/**
- * Created by Sagar
- */
+
 public class DataManager {
 
     public BaseApiManager mBaseApiManager = new BaseApiManager();
@@ -28,5 +45,12 @@ public class DataManager {
      */
     public Observable<DetailAnnouncement> getAnnouncementDetail(String id) {
         return mBaseApiManager.getTavernaApi().getAnnouncement(id);
+    }
+
+    /**
+     * @return List of all Workflow
+     */
+    public Observable<Workflows> getAllWorkflow(int pageNumber){
+        return mBaseApiManager.getTavernaApi().getAllWorkflows(pageNumber);
     }
 }
