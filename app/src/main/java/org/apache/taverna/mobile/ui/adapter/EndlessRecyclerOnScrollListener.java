e73946a7d77fb36464f0c08d7bf26d5ac8253a98
Sagar <kumarsagar15795@gmail.com>
2016-06-23 01:52:41 +0530

    change visibleThreshold to 5

--- app/src/main/java/org/apache/taverna/mobile/ui/adapter/EndlessRecyclerOnScrollListener.java
+++ app/src/main/java/org/apache/taverna/mobile/ui/adapter/EndlessRecyclerOnScrollListener.java
@@ -28,7 +28,7 @@ public abstract class EndlessRecyclerOnScrollListener extends RecyclerView.OnScr
     private int previousTotal = 0; // The total number of items in the dataset after the last load
     private boolean loading = true; // True if we are still waiting for the last set of data to
     // load.
-    private int visibleThreshold = 1; // The minimum amount of items to have below your current
+    private int visibleThreshold = 5; // The minimum amount of items to have below your current
     // scroll position before loading more.
     private int current_page = 1;
 
