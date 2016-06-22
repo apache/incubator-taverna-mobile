f8307777fbbcd0c0fc05f8ca5e763db49eb28527
Sagar <kumarsagar15795@gmail.com>
2016-06-23 00:06:26 +0530

    remove checkstyle error

--- app/src/main/java/org/apache/taverna/mobile/utils/SvgSoftwareLayerSetter.java
+++ app/src/main/java/org/apache/taverna/mobile/utils/SvgSoftwareLayerSetter.java
@@ -20,7 +20,8 @@ import android.widget.ImageView;
 public class SvgSoftwareLayerSetter<T> implements RequestListener<T, PictureDrawable> {
 
     @Override
-    public boolean onException(Exception e, T model, Target<PictureDrawable> target, boolean isFirstResource) {
+    public boolean onException(Exception e, T model, Target<PictureDrawable> target,
+                               boolean isFirstResource) {
         ImageView view = ((ImageViewTarget<?>) target).getView();
         if (Build.VERSION_CODES.HONEYCOMB <= Build.VERSION.SDK_INT) {
             view.setLayerType(ImageView.LAYER_TYPE_NONE, null);
@@ -29,8 +30,8 @@ public class SvgSoftwareLayerSetter<T> implements RequestListener<T, PictureDraw
     }
 
     @Override
-    public boolean onResourceReady(PictureDrawable resource, T model, Target<PictureDrawable> target,
-                                   boolean isFromMemoryCache, boolean isFirstResource) {
+    public boolean onResourceReady(PictureDrawable resource, T model
+            , Target<PictureDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
         ImageView view = ((ImageViewTarget<?>) target).getView();
         if (Build.VERSION_CODES.HONEYCOMB <= Build.VERSION.SDK_INT) {
             view.setLayerType(ImageView.LAYER_TYPE_SOFTWARE, null);
