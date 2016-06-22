72be86d931120744efc2767a6d99979bb5785242
Sagar <kumarsagar15795@gmail.com>
2016-06-23 01:56:22 +0530

    add load more adapter logic

--- app/src/main/java/org/apache/taverna/mobile/ui/adapter/WorkflowAdapter.java
+++ app/src/main/java/org/apache/taverna/mobile/ui/adapter/WorkflowAdapter.java
@@ -22,6 +22,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.io.InputStream;
@@ -31,9 +32,16 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class WorkflowAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private final int VIEW_ITEM = 1;
+    private final int VIEW_PROG = 0;
+
     private static final String TAG = WorkflowAdapter.class.getName();
+
     private final List<DetailWorkflow> detailWorkflowList;
+
     private final Context context;
+
     private GenericRequestBuilder<Uri, InputStream, SVG, PictureDrawable> requestBuilder;
 
     public WorkflowAdapter(List<DetailWorkflow> detailWorkflowList, Context context) {
@@ -48,6 +56,7 @@ public class WorkflowAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder vh;
         requestBuilder = Glide.with(context)
                 .using(Glide.buildStreamModelLoader(Uri.class, context), InputStream.class)
                 .from(Uri.class)
@@ -60,9 +69,18 @@ public class WorkflowAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
                 .error(R.drawable.tavernalogo)
                 .animate(android.R.anim.fade_in)
                 .listener(new SvgSoftwareLayerSetter<Uri>());
+        if (viewType == VIEW_ITEM) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(
+                    R.layout.item_recyclerview_dashboard, parent, false);
 
-        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(
-                R.layout.item_recyclerview_dashboard, parent, false));
+            vh = new ViewHolder(v);
+        } else {
+            View v = LayoutInflater.from(parent.getContext()).inflate(
+                    R.layout.item_progressbar, parent, false);
+
+            vh = new ProgressViewHolder(v);
+        }
+        return vh;
     }
 
     @Override
@@ -87,6 +105,11 @@ public class WorkflowAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
         return detailWorkflowList.size();
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        return detailWorkflowList.get(position) != null ? VIEW_ITEM : VIEW_PROG;
+    }
+
 
     class ViewHolder extends RecyclerView.ViewHolder {
 
@@ -104,6 +127,15 @@ public class WorkflowAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
             super(itemView);
             ButterKnife.bind(this, itemView);
 
+        }
+    }
+    public static class ProgressViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.progressBar1)
+        public ProgressBar progressBar;
+
+        public ProgressViewHolder(View v) {
+            super(v);
+            ButterKnife.bind(this, v);
         }
     }
 }
