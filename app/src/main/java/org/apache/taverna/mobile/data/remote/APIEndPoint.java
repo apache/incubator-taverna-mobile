f3eb271d736454307baa6afc98e4296199e3b715
Sagar <kumarsagar15795@gmail.com>
2016-06-24 16:10:07 +0530

    add APIEndPoint class

---
+++ app/src/main/java/org/apache/taverna/mobile/data/remote/APIEndPoint.java
@@ -0,0 +1,11 @@
+package org.apache.taverna.mobile.data.remote;
+
+//This class contains all the Constants for API End Points
+
+public class APIEndPoint {
+    public static final String ALLANNOUNCEMENT = "announcements.xml";
+    public static final String ANNOUNCEMENT = "announcement.xml";
+    public static final String ALLWORKFLOW = "workflows.xml";
+    public static final String WORKFLOW = "workflow.xml";
+
+}
