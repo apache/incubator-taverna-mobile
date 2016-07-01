2b416fe68956cae1b6b6a572dfc374d9c9accb4e
Sagar <kumarsagar15795@gmail.com>
2016-07-01 23:39:39 +0530

    add overflow menu

--- app/src/main/java/org/apache/taverna/mobile/ui/workflowdetail/WorkflowDetailFragment.java
+++ app/src/main/java/org/apache/taverna/mobile/ui/workflowdetail/WorkflowDetailFragment.java
@@ -15,6 +15,9 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
@@ -118,6 +121,27 @@ public class WorkflowDetailFragment extends Fragment implements WorkflowDetailMv
             mProgressBar.setVisibility(View.GONE);
             showErrorSnackBar(getString(R.string.no_internet));
         }
+
+        setHasOptionsMenu(true);
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_workflow_detail, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.licence:
+
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
     }
 
     @Override
