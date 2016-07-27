2b1f8eaee9448f1accf5644005fe4c8ff0f0ea98
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2016-07-27 12:11:49 +0100

    Unnecessary boolean when checking for SDK level removed

--- app/src/main/java/org/apache/taverna/mobile/activities/SettingsActivity.java
+++ app/src/main/java/org/apache/taverna/mobile/activities/SettingsActivity.java
@@ -54,13 +54,6 @@ import java.util.List;
  */
 public class SettingsActivity extends PreferenceActivity {
     /**
-     * Determines whether to always show the simplified settings UI, where
-     * settings are presented in a single list. When false, settings are shown
-     * as a master/detail two-pane view on tablets. When true, a single pane is
-     * shown on tablets.
-     */
-    private static final boolean ALWAYS_SIMPLE_PREFS = false;
-    /**
      * A preference value change listener that updates the preference's summary
      * to reflect its new value.
      */
@@ -102,14 +95,12 @@ public class SettingsActivity extends PreferenceActivity {
 
     /**
      * Determines whether the simplified settings UI should be shown. This is
-     * true if this is forced via {@link #ALWAYS_SIMPLE_PREFS}, or the device
-     * doesn't have newer APIs like {@link PreferenceFragment}, or the device
-     * doesn't have an extra-large screen. In these cases, a single-pane
+     * true if the device doesn't have newer APIs like {@link PreferenceFragment},
+     * or the device doesn't have an extra-large screen. In these cases, a single-pane
      * "simplified" settings UI should be shown.
      */
     private static boolean isSimplePreferences(Context context) {
-        return ALWAYS_SIMPLE_PREFS
-                || Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB
                 || !isXLargeTablet(context);
     }
 
