2c68a32eb0ef61c7abf4f2bbd1d657a8cf801845
Sagar <kumarsagar15795@gmail.com>
2016-08-02 23:48:58 +0530

    add licence header

--- app/src/main/java/org/apache/taverna/mobile/activities/WorkflowDetailActivity.java
+++ app/src/main/java/org/apache/taverna/mobile/activities/WorkflowDetailActivity.java
@@ -1,35 +1,23 @@
-package org.apache.taverna.mobile.activities;
-
-/**
- * Apache Taverna Mobile
- * Copyright 2015 The Apache Software Foundation
- *
- * This product includes software developed at
- * The Apache Software Foundation (http://www.apache.org/).
- *
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
+ * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
+ * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
+ * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
+package org.apache.taverna.mobile.activities;
 
-import org.apache.taverna.mobile.R;
-import org.apache.taverna.mobile.fragments.workflowdetails.WorkflowAboutFragment;
-import org.apache.taverna.mobile.fragments.workflowdetails.WorkflowLicenceFragment;
-import org.apache.taverna.mobile.fragments.workflowdetails.WorkflowRunHistoryFragment;
-import org.apache.taverna.mobile.fragments.workflowdetails.WorkflowdetailFragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -39,6 +27,12 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.apache.taverna.mobile.R;
+import org.apache.taverna.mobile.fragments.workflowdetails.WorkflowAboutFragment;
+import org.apache.taverna.mobile.fragments.workflowdetails.WorkflowLicenceFragment;
+import org.apache.taverna.mobile.fragments.workflowdetails.WorkflowRunHistoryFragment;
+import org.apache.taverna.mobile.fragments.workflowdetails.WorkflowdetailFragment;
 
 import java.util.Locale;
 
--- app/src/main/java/org/apache/taverna/mobile/ui/login/LoginActivity.java
+++ app/src/main/java/org/apache/taverna/mobile/ui/login/LoginActivity.java
@@ -1,29 +1,22 @@
-package org.apache.taverna.mobile.ui.login;
-
 /*
-* Apache Taverna Mobile
-* Copyright 2015 The Apache Software Foundation
-
-* This product includes software developed at
-* The Apache Software Foundation (http://www.apache.org/).
-
-* Licensed to the Apache Software Foundation (ASF) under one
-* or more contributor license agreements. See the NOTICE file
-* distributed with this work for additional information
-* regarding copyright ownership. The ASF licenses this file
-* to you under the Apache License, Version 2.0 (the
-* "License"); you may not use this file except in compliance
-* with the License. You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied. See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.taverna.mobile.ui.login;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -31,6 +24,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import org.apache.taverna.mobile.R;
+import org.apache.taverna.mobile.utils.ActivityUtils;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -42,9 +36,8 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new LoginFragment())
-                    .commit();
+            ActivityUtils.addFragmentToActivity(getSupportFragmentManager(),new LoginFragment(),R
+                    .id.container);
         }
     }
 
