c70f85d94b18f3c560fa2d890e5c1d49a3a0d7d2
Sagar <kumarsagar15795@gmail.com>
2016-09-24 11:17:18 +0530

    add multiple converter

--- app/src/main/java/org/apache/taverna/mobile/data/remote/BaseApiManager.java
+++ app/src/main/java/org/apache/taverna/mobile/data/remote/BaseApiManager.java
@@ -20,6 +20,7 @@ package org.apache.taverna.mobile.data.remote;
 
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
 
@@ -30,9 +31,9 @@ public class BaseApiManager {
 
     public static final String TAVERNA_PLAYER_END_POINT = "http://139.59.28.12:3000/";
 
-    /******** Helper class that sets up a new services *******/
+    /******** Helper class that sets up a new services with simplexml converter factory *******/
 
-    private <T> T createApi(Class<T> clazz, String ENDPOINT) {
+    private <T> T createSimpleXMLApi(Class<T> clazz, String ENDPOINT) {
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(ENDPOINT)
@@ -44,11 +45,25 @@ public class BaseApiManager {
         return retrofit.create(clazz);
     }
 
+    /******** Helper class that sets up a new services with gson converter factory *******/
+
+    private <T> T createJsonApi(Class<T> clazz, String ENDPOINT) {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(ENDPOINT)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .client(new TavernaOkHttpClient().getTavernaOkHttpClient())
+                .build();
+
+        return retrofit.create(clazz);
+    }
+
     public TavernaService getTavernaApi() {
-        return createApi(TavernaService.class, MY_EXPERIMENT_END_POINT);
+        return createSimpleXMLApi(TavernaService.class, MY_EXPERIMENT_END_POINT);
     }
 
     public  TavernaPlayerService getTavernaPlayerApi(){
-        return createApi(TavernaPlayerService.class, TAVERNA_PLAYER_END_POINT);
+        return createJsonApi(TavernaPlayerService.class, TAVERNA_PLAYER_END_POINT);
     }
 }
