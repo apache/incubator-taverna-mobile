08bdb7452800efecd2873b738217445c1d3a27bd
Sagar <kumarsagar15795@gmail.com>
2016-09-24 11:14:58 +0530

    add json accept header constant

--- app/src/main/java/org/apache/taverna/mobile/data/remote/APIEndPoint.java
+++ app/src/main/java/org/apache/taverna/mobile/data/remote/APIEndPoint.java
@@ -32,6 +32,7 @@ public class APIEndPoint {
     public static final String WHOAMI = "whoami.xml";
 
     public static final String XML_ACCEPT_HEADER = "Accept: application/xml";
+    public static final String JSON_ACCEPT_HEADER = "Accept: application/json";
     public static final String JSON_CONTENT_HEADER = "Content-Type: application/json";
     public static final String UTF_CONTENT_ENCODING_HEADER = "Content-Encoding: UTF-8";
 }
