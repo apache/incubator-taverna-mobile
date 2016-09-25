d16d467d4b4b4954e45e40edee65d9dd8ed53418
Sagar <kumarsagar15795@gmail.com>
2016-09-25 21:53:30 +0530

    add methods in player service

--- app/src/main/java/org/apache/taverna/mobile/data/remote/TavernaPlayerService.java
+++ app/src/main/java/org/apache/taverna/mobile/data/remote/TavernaPlayerService.java
@@ -1,12 +1,17 @@
 package org.apache.taverna.mobile.data.remote;
 
 
+import org.apache.taverna.mobile.data.model.PlayerWorkflow;
+import org.apache.taverna.mobile.data.model.PlayerWorkflowDetail;
+
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 import rx.Observable;
 
 public interface TavernaPlayerService {
@@ -14,8 +19,21 @@ public interface TavernaPlayerService {
     @POST("/workflows.json")
     @Headers({
             APIEndPoint.JSON_CONTENT_HEADER,
+            APIEndPoint.JSON_ACCEPT_HEADER,
             APIEndPoint.UTF_CONTENT_ENCODING_HEADER})
-    Observable<ResponseBody> uploadWorkflow(@Body RequestBody body, @Header("Authorization") String
+    Observable<PlayerWorkflow> uploadWorkflow(@Body RequestBody body, @Header("Authorization") String
             authorization);
 
+    @POST("/users/sign_in")
+    @Headers({
+            APIEndPoint.XML_ACCEPT_HEADER})
+    Observable<ResponseBody> playerlogin(@Header("Authorization") String
+                                                 authorization);
+
+
+    @GET("/runs/new")
+    @Headers({
+            APIEndPoint.JSON_CONTENT_HEADER,
+            APIEndPoint.JSON_ACCEPT_HEADER})
+    Observable<PlayerWorkflowDetail> getWorkflowDetail(@Query("workflow_id") int id);
 }
