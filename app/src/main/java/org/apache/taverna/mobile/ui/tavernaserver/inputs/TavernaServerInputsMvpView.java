825c79b11ee3895c4f03fe2fc071c59c9de26f6c
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2017-01-11 15:19:34 +0000

    Change wording on create run page

---
+++ app/src/main/java/org/apache/taverna/mobile/ui/tavernaserver/inputs/TavernaServerInputsMvpView.java
@@ -0,0 +1,10 @@
+package org.apache.taverna.mobile.ui.tavernaserver.inputs;
+
+import org.apache.taverna.mobile.ui.base.MvpView;
+
+/**
+ * Created by ian on 16/12/16.
+ */
+
+public interface TavernaServerInputsMvpView extends MvpView {
+}
---
+++ app/src/main/java/org/apache/taverna/mobile/ui/tavernaserver/inputs/TavernaServerInputsPresenter.java
@@ -0,0 +1,10 @@
+package org.apache.taverna.mobile.ui.tavernaserver.inputs;
+
+import org.apache.taverna.mobile.ui.base.BasePresenter;
+
+/**
+ * Created by ian on 16/12/16.
+ */
+
+public class TavernaServerInputsPresenter extends BasePresenter<TavernaServerInputsMvpView> {
+}
