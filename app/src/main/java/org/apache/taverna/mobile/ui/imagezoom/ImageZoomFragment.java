dea42ee6b18de1ae907fe1779351a8387a999cfe
saketkumar95 <saket.singh1410@gmail.com>
2018-03-04 14:17:36 +0530

    Adds image option to download,share and open in browser

--- app/src/main/java/org/apache/taverna/mobile/ui/imagezoom/ImageZoomFragment.java
+++ app/src/main/java/org/apache/taverna/mobile/ui/imagezoom/ImageZoomFragment.java
@@ -19,13 +19,23 @@
 package org.apache.taverna.mobile.ui.imagezoom;
 
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,6 +52,16 @@ import com.bumptech.glide.request.target.Target;
 import org.apache.taverna.mobile.R;
 import org.apache.taverna.mobile.utils.ConnectionInfo;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -55,12 +75,21 @@ public class ImageZoomFragment extends Fragment implements ImageZoomMvpView {
 
     private static final String SERVER_ERROR = "Sever Error. Please try after sometime";
 
+    int id = 1;
+
+    private NotificationManager mNotifyManager;
+
+    private NotificationCompat.Builder mBuilder;
+
 
     @BindView(R.id.ivWorkflowImage)
     ImageView workflowImage;
 
     @BindView(R.id.ivClose)
     ImageView close;
+
+    @BindView(R.id.ivdots)
+    ImageView options;
 
     PhotoViewAttacher mAttacher;
 
@@ -119,6 +148,50 @@ public class ImageZoomFragment extends Fragment implements ImageZoomMvpView {
         getActivity().overridePendingTransition(R.anim.slide_in_down, R.anim.slide_out_down);
 
     }
+
+    @OnClick(R.id.ivdots)
+    public void openOptions(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getAppContext());
+        builder.setItems(R.array.workflow_images_options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        downloadImage(svgURI);
+                        break;
+                    case 1:
+                        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+                        } else {
+                            shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+                            shareIntent.setType("image/*");
+                            shareIntent.putExtra(Intent.EXTRA_STREAM, svgURI);
+                            startActivity(Intent.createChooser(shareIntent, "Share via"));
+                        }
+                        break;
+                    case 2:
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(svgURI));
+                        startActivity(browserIntent);
+                        break;
+                }
+
+            }
+        });
+        builder.show();
+    }
+
+    public void downloadImage(String url) {
+        mNotifyManager = (NotificationManager) getAppContext()
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        mBuilder = new NotificationCompat.Builder(getAppContext());
+        mBuilder.setContentTitle(url)
+                .setSmallIcon(android.R.drawable.stat_sys_download);
+        DownloadImage download = new DownloadImage();
+        download.execute(url);
+    }
+
+
 
     @Override
     public void showErrorSnackBar(String error) {
@@ -224,5 +297,90 @@ public class ImageZoomFragment extends Fragment implements ImageZoomMvpView {
     public void onDestroyView() {
         super.onDestroyView();
         mImageZoomPresenter.detachView();
+    }
+
+    public class DownloadImage extends AsyncTask<String, Integer, String> {
+
+        int calculatedProgress = 0;
+        int imageLength = 0;
+
+        @Override
+        protected void onPreExecute() {
+            mBuilder.setProgress(100, 0, false);
+            mNotifyManager.notify(id, mBuilder.build());
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String path = params[0];
+            File downloadedImages = new File(
+                    Environment
+                            .getExternalStoragePublicDirectory(
+                                    Environment
+                                            .DIRECTORY_DOWNLOADS)
+                            .getAbsolutePath() + "/" + Uri.parse(path)
+                            .getLastPathSegment());
+            int total = 0;
+            int count = 0;
+
+            try {
+                URL url = new URL(path);
+                URLConnection urlConnection = url.openConnection();
+                urlConnection.connect();
+                imageLength = urlConnection.getContentLength();
+                boolean isFileCreated = downloadedImages.exists();
+                InputStream inputStream = new BufferedInputStream(url.openStream(), 8192);
+                byte[] data = new byte[1024];
+                OutputStream outputStream = new FileOutputStream(downloadedImages);
+
+                if (!isFileCreated) {
+                    isFileCreated = downloadedImages.createNewFile();
+                }
+                while ((count = inputStream.read(data)) != -1) {
+                    total = total + count;
+                    outputStream.write(data, 0, count);
+                    calculatedProgress = (int) total * 100 / imageLength;
+                    publishProgress(total);
+                }
+                inputStream.close();
+                outputStream.close();
+
+            } catch (MalformedURLException e) {
+
+            } catch (IOException e) {
+
+            }
+
+            return String.valueOf(downloadedImages);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+
+            mBuilder.setProgress(100, values[0], true);
+            mNotifyManager.notify(id, mBuilder.build());
+            super.onProgressUpdate(values);
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            mBuilder.setContentText("Download complete");
+            mBuilder.setSmallIcon(android.R.drawable.stat_sys_download_done);
+            mBuilder.setProgress(0, 0, false);
+            File file = new File(result);
+            Uri uri = Uri.fromFile(file);
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "image/*");
+            PendingIntent pIntent = PendingIntent.getActivity(getAppContext(), 0, intent, 0);
+            mBuilder.setContentIntent(pIntent).build();
+            mBuilder.setAutoCancel(true);
+            mNotifyManager.notify(id, mBuilder.build());
+
+        }
     }
 }
